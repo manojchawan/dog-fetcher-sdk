286034f8d393467e0bccd4e58b0de7c25cafaf45
Manoj Chvan <manojchvan@ManojC-LT.local>
2024-06-13 20:37:25 +0530

    add framework

---
+++ DogFetcher/DogFetcher/DogFetcher.h
@@ -0,0 +1,18 @@
+//
+//  DogFetcher.h
+//  DogFetcher
+//
+//  Created by Manoj Chvan on 13/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DogFetcher.
+FOUNDATION_EXPORT double DogFetcherVersionNumber;
+
+//! Project version string for DogFetcher.
+FOUNDATION_EXPORT const unsigned char DogFetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DogFetcher/PublicHeader.h>
+
+
